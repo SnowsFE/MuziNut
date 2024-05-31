672627c33ba601ea938c6db34ef1fe8cab76b0b1
SnowsFE <snoerkr@gmail.com>
2024-05-31 14:29:53 +0900

    로고 이미지 제작

--- front/src/app/components/navbar.tsx
+++ front/src/app/components/navbar.tsx
@@ -1,9 +1,35 @@
+"use client";
 import React from "react";
-
+import styled from "styled-components";
 export const navbar = () => {
   return (
-    <div>
-      <p>navbar</p>
-    </div>
+    <Navcontainer>
+      <Navbar>
+        <p>뮤지넛</p>
+      </Navbar>
+    </Navcontainer>
   );
 };
+
+export default navbar;
+
+const Navcontainer = styled.div`
+  background-color: var(--color-bg-01);
+  color: var(--color-content-02);
+  height: 60px;
+  left: 0;
+  min-width: 800px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  z-index: 11000;
+`;
+
+const Navbar = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100%;
+  justify-content: flex-end;
+  padding-right: 20px;
+  width: 100%;
+`;
