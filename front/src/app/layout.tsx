1c07ae75b56baab604153f5eb187b51ceaf8e93a
SnowsFE <snoerkr@gmail.com>
2024-06-03 17:40:55 +0900

    네비게이션 바 제작

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -17,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
