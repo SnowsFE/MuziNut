bd6a3562623f0ad42cca361f9b99ecf987c1e28d
SnowsFE <snoerkr@gmail.com>
2024-06-17 17:34:42 +0900

    넛츠 결제페이지 구현중

--- front/src/app/(_Front)/profile/nuts/page.tsx
+++ front/src/app/(_Front)/profile/nuts/page.tsx
@@ -4,10 +4,10 @@ import styled from "styled-components";
 import Image from "next/image";
 import Login from "../../../../../public/images/login.png";
 import banner from "../../../../../public/images/banner.png";
-import threedot from "../../../../../public/svgs/threedot.svg";
+import nut from "../../../../../public/images/Nuts.png";
 import Link from "next/link";
-import { LikeIcon, CommentIcon } from "../../../../app/components/icon";
-import { Userdata, CommentData } from "../userdata";
+import { Userdata } from "../userdata";
+import { VoteBox } from "@/app/components/icon";
 
 // UseridProps를 props로 받습니다.
 const UseridProfile: React.FC = () => {
@@ -65,6 +65,38 @@ const UseridProfile: React.FC = () => {
           </StyledLink>
         </SelectContainer>
       </SelectBar>
+      <NutsContainer>
+        <NutsTitle>내 넛츠</NutsTitle>
+        <NutsBodyContainer>
+          <NutsVotes>
+            <Nuts>
+              <NutsImage>
+                <Image src={nut} alt="Nuts"></Image>
+              </NutsImage>
+              <MyNuts>보유중인 넛츠 n개</MyNuts>
+              <NutsCharge>
+                <p>충전하기</p>
+              </NutsCharge>
+            </Nuts>
+            <Votes>
+              <VotesImage>
+                <VoteBox />
+              </VotesImage>
+
+              <MyVotes>보유중인 투표권 n개</MyVotes>
+              <VotesCharge>
+                <p>충전하기</p>
+              </VotesCharge>
+            </Votes>
+          </NutsVotes>
+          <NutsReceipt>
+            <ul>
+              <li>사용 내역</li>
+              <li>구매 내역</li>
+            </ul>
+          </NutsReceipt>
+        </NutsBodyContainer>
+      </NutsContainer>
     </ProfileContainer>
   );
 };
@@ -194,9 +226,127 @@ const StyledLink = styled(Link)`
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------
-// 라운지를 감싸는 큰 컨테이너
-const Lounge = styled.div`
+// 넛츠를 감싸는 큰 컨테이너
+const NutsContainer = styled.div`
   padding-right: calc(50% - 642px);
   padding-left: calc(50% - 642px);
   padding-top: 16px;
+`;
+
+// 넛츠 페이지 제목
+const NutsTitle = styled.div`
+  padding: 10px 0;
+`;
+
+// 넛츠 바디 컨테이너
+const NutsBodyContainer = styled.div`
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 30px;
+`;
+
+// 넛츠, 투표권 컨테이너
+const NutsVotes = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 30px;
+  gap: 20px;
+`;
+
+// 넛츠 컨테이너 --------------------
+
+// 내 넛츠
+const Nuts = styled.div`
+  width: 50%;
+  display: flex;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 15px 20px;
+  align-items: center;
+  gap: 15px;
+  box-shadow: 0 2px 30px 0 rgba(0, 0, 0, 0.06);
+`;
+
+// 넛츠 이미지
+const NutsImage = styled.div`
+  img {
+    width: 36px;
+    height: 36px;
+    margin-top: 1px;
+  }
+`;
+
+// 넛츠 충전하기
+const NutsCharge = styled.div`
+  display: flex;
+  margin-left: auto;
+  border: 1px;
+  border-radius: 5px;
+  padding: 0 30px;
+  background-color: #16be78;
+  cursor: pointer;
+  p {
+    color: white;
+  }
+  &:hover {
+    background-color: #1bb373;
+  }
+`;
+
+const MyNuts = styled.div``;
+
+// 넛츠 컨테이너 --------------------
+
+// 투표권 컨테이너 --------------------
+
+// 내 투표권
+const Votes = styled.div`
+  width: 50%;
+  display: flex;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 15px 20px;
+  align-items: center;
+  gap: 15px;
+  box-shadow: 0 2px 30px 0 rgba(0, 0, 0, 0.06);
+`;
+
+// 투표권 이미지
+const VotesImage = styled.div`
+  margin-bottom: 7px;
+`;
+
+// 투표권 충전하기
+const VotesCharge = styled.div`
+  margin-left: auto;
+  border: 1px;
+  border-radius: 5px;
+  padding: 0 30px;
+  background-color: #16be78;
+  cursor: pointer;
+  p {
+    color: white;
+  }
+  &:hover {
+    background-color: #1bb373;
+  }
+`;
+
+const MyVotes = styled.div``;
+
+// 투표권 컨테이너 --------------------
+
+// 넛츠 영수증 [사용내역, 구매내역 + 세부사항]
+const NutsReceipt = styled.div`
+  padding: 4px 4px;
+  font-size: 18px;
+  border-bottom: 1px solid #ccc;
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    gap: 1.75rem;
+  }
 `;
