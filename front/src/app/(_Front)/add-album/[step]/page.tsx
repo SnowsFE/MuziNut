9630977430c32f78c8d1761bb40ced5748406a08
G1Coding <32190250@dankook.ac.kr>
2024-07-22 12:30:15 +0900

    [서버 연동 완료] 앨범 업로드 완성

--- front/src/app/(_Front)/add-album/[step]/page.tsx
+++ front/src/app/(_Front)/add-album/[step]/page.tsx
@@ -7,22 +7,29 @@ import AlbumForm from "@/app/components/add-album/album";
 import SongForm, { Song } from "@/app/components/add-album/song";
 import StepForCheck from "@/app/components/add-album/check";
 import SubmitModal from "@/app/components/add-album/submitModal";
+import axios from "axios";
 
 export default function AddAlbumModal() {
   const [step, setStep] = useState(0);
+  const [title, setTitle] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [albumErrors, setAlbumErrors] = useState<{
+    title: string;
+    description: string;
+  }>({ title: "", description: "" });
+  const [songs, setSongs] = useState<Song[]>([]); // 곡 정보 배열 상태
+  const [imageClick, setImageClick] = useState(false); // 이미지 클릭 시 imageClick 상태변경
+  const [isHovered, setIsHovered] = useState(false); // 이미지 hover 여부를 나타내는 상태
+  const [isChecked, setIsChecked] = useState(false); // isChecked 상태 추가
+  const [showModal, setShowModal] = useState(false); // 체크박스 선택하고 제출 눌렀을 때 모달창
+  const [albumImg, setAlbumImg] = useState<File | null>(null);
 
   useEffect(() => {
     // URL 업데이트
     window.history.pushState(null, "", `/add-album?step=${step + 1}`);
   }, [step]);
 
-  // 버튼 클릭 시 step 상태 변경
-  /*const isButtonClicked = () => {
-    if (step < 2 && validateAlbumForm()) {
-      setStep((preStep) => preStep + 1);
-    }
-  };
-  */
+  // 다음 버튼 클릭 시 step 이동
   const isButtonClicked = () => {
     if (step === 0 && validateAlbumForm()) {
       setStep(step + 1);
@@ -30,20 +37,13 @@ export default function AddAlbumModal() {
       setStep(step + 1);
     }
   };
+  // 이전 버튼 클릭 시 step 이동
   const isButtonClickedBack = () => {
     if (step > 0) {
       setStep(step - 1);
     }
   };
 
-  // ===================================
-  // 첫 번째 form의 상태
-  const [title, setTitle] = useState<string>("");
-  const [description, setDescription] = useState<string>("");
-  const [albumErrors, setAlbumErrors] = useState<{
-    title: string;
-    description: string;
-  }>({ title: "", description: "" });
   //유효성 검증
   const validateAlbumForm = (): boolean => {
     let valid = true;
@@ -59,68 +59,131 @@ export default function AddAlbumModal() {
     setAlbumErrors(newErrors);
     return valid;
   };
-  // 보낼 데이터
-  const dataSubmit = () => {
-    console.log("보낼 Form Data:", { title, description });
-  };
+
   // 두 번쨰 form 의 상태
-  const [songs, setSongs] = useState<Song[]>([]); // 곡 정보 배열 상태
   const handleSongsChange = (updatedSongs: Song[]) => {
     console.log("업데이트된 곡 리스트:", updatedSongs);
     setSongs(updatedSongs); // 곡 정보 배열 상태 업데이트
   };
-  const handleNextStep = () => {
-    console.log("다음 버튼 클릭 - 현재 곡 리스트:", songs);
-    // 다음 로직 수행
-  };
 
-  //서버로 보낼 데이터
-  // 앨범 및 곡 데이터를 서버로 전송하는 함수
-  const sendToServer = () => {
-    if (!validateAlbumForm()) {
-      console.log("앨범 폼 데이터가 유효하지 않습니다.");
-      return;
-    }
-    const dataToSend = {
-      title,
-      description,
-      songs,
-    };
-    console.log("서버로 전송할 데이터:", dataToSend);
-  };
-
-  //============================================================================
-
-  // 이미지 클릭 시 imageClick 상태변경
-  const [imageClick, setImageClick] = useState(false);
-
-  const isImageClicked = () => {
-    setImageClick(true);
-  };
-
-  // 이미지 hover 여부를 나타내는 상태
-  const [isHovered, setIsHovered] = useState(false);
-
-  const [isChecked, setIsChecked] = useState(false); // isChecked 상태 추가
   // 체크박스 변경 핸들러
   const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setIsChecked(e.target.checked);
   };
 
-  // 체크박스 선택하고 제출 눌렀을 때 모달창
-  const [showModal, setShowModal] = useState(false);
-
+  // 제출하기 버튼 눌렸을 때
   const handleSubmit = () => {
     if (!isChecked) {
       alert("위 사실에 대해 모두 인지하고, 동의해 주세요.");
     } else {
       console.log("모달 열려야 함");
       setShowModal(true);
+      sendToServer();
+    }
+  };
+  // 모달 창 닫기(곡 수록하는)
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setAlbumImg(e.target.files[0]);
     }
   };
 
-  const closeModal = () => {
-    setShowModal(false);
+  //서버로 보낼 데이터
+  // 앨범 및 곡 데이터를 서버로 전송하는 함수
+  const sendToServer = async () => {
+    if (!validateAlbumForm()) {
+      console.log("앨범 폼 데이터가 유효하지 않습니다.");
+      return;
+    }
+    const albumData = {
+      albumName: title,
+      albumBio: description,
+      songs: songs.map((song) => ({
+        songName: song.songName,
+        lyricist: song.lyricist,
+        composer: song.composer,
+        lyrics: song.lyrics,
+        genres: song.genres,
+        originFilename: song.originFilename,
+      })),
+    };
+
+    const formData = new FormData();
+    if (albumImg) {
+      formData.append("albumImg", albumImg);
+    }
+    songs.forEach((song) => {
+      console.log("songfile 붙이기!!!!!!!!!!!!!!!!");
+      if (song.file) {
+        formData.append("songFiles", song.file);
+      }
+    });
+
+    // songs.forEach((song, index) => {
+    //   if (song.file) {
+    //     formData.append(`songfiles[${index}]`, song.file);
+    //   }
+    // });
+
+    // JSON 데이터를 Blob으로 변환하여 FormData에 추가
+    const albumDataBlob = new Blob([JSON.stringify(albumData)], {
+      type: "application/json",
+    });
+    formData.append("albumData", albumDataBlob);
+    // formData.append('albumData', JSON.stringify(albumData));
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/album/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            //나중에 이 부분 공통 컴포넌트화하기
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+        }
+      );
+      if (response.status === 200) {
+        console.log("!!!!!!!앨범 데이터 전송 성공!!!!!!!");
+        console.log("응답 데이터====", response.data);
+      }
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error) && error.response) {
+        console.log("서버로부터 받은 에러 데이터", error.response.data);
+        console.log("서버로부터 받은 에러 상태코드", error.response.status);
+        console.log("서버로부터 받은 에러 headers", error.response.headers);
+
+        if (error.response.status === 503) {
+          alert("앨범 등록에 실패하였습니다.");
+          return;
+        } else {
+          alert("[error] 서버와 통신 오류 발생.");
+        }
+      } else {
+        //axios 에러가 아닌 다른 예외가 발생한 경우
+        alert("[error] 오류가 발생했습니다. 다시 시도해주세요.");
+      }
+    }
+  };
+  // 보낼 데이터
+  const dataSubmit = () => {
+    console.log("보낼 Form Data:", { title, description });
+  };
+
+  const handleNextStep = () => {
+    console.log("다음 버튼 클릭 - 현재 곡 리스트:", songs);
+    // 다음 로직 수행
+  };
+
+  //============================================================================
+
+  const isImageClicked = () => {
+    setImageClick(true);
   };
 
   return (
@@ -182,6 +245,13 @@ export default function AddAlbumModal() {
                 <div className={styles.image__over__text}>
                   <span>이미지를 클릭해서 Form을 입력해보세요!</span>
                 </div>
+              )}
+              {imageClick && (
+                <input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageChange}
+                />
               )}
             </div>
           )}
@@ -253,7 +323,6 @@ export default function AddAlbumModal() {
                 <button
                   onClick={() => {
                     handleSubmit();
-                    sendToServer();
                   }}
                   className={`${styles.submit__btn} ${
                     isChecked ? styles.active : ""
--- front/src/app/components/add-album/song.tsx
+++ front/src/app/components/add-album/song.tsx
@@ -9,12 +9,13 @@ import musicIcon from "@/../../public/svgs/music_note.svg";
 
 // modal에 들어갈 내용
 export type Song = {
-  title: string;
+  songName: string;
   lyricist: string; //작사가
-  songwriter: string; //작곡가
+  composer: string; //작곡가
   lyrics: string;
   genres: string[]; //최대 3개까지
-  mp3File: File | null;
+  originFilename: string;
+  file: File | null;
 };
 interface SongFormProps {
   onSongsChange: (songs: Song[]) => void; // 콜백 함수 prop 추가
@@ -37,15 +38,15 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
     setShowModal(false);
   };
 
-  const [title, setTitle] = useState("");
+  const [songName, setSongName] = useState("");
   const [lyricist, setLyricist] = useState("");
-  const [songwriter, setSongwriter] = useState("");
+  const [composer, setComposer] = useState("");
   const [lyrics, setLyrics] = useState("");
   const [genres, setGenres] = useState<string[]>([]);
-  const [mp3File, setMp3File] = useState<File | null>(null);
-
+  const [originFilename, setOriginFilename] = useState("");
   // 백엔드로 정보 보낼 때 --- 각 곡의 정보를 배열로 감싸서
   const [songs, setSongs] = useState<Song[]>([]);
+  const [file, setFile] = useState<File | null>(null);
 
   // 사용자가 선택할 수 있는 장르들
   const genreListView = [
@@ -59,6 +60,17 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
     "힙합",
     "버튜버",
   ];
+  const genreMap: { [key: string]: string } = {
+    케이팝: "KPOP",
+    발라드: "BALLAD",
+    알앤비: "RNB",
+    트로트: "TROT",
+    기타: "ETC",
+    팝송: "POP",
+    인디: "INDIE",
+    힙합: "HIPHOP",
+    버튜버: "VIRTUBER",
+  };
 
   /* 장르 개수 제한 3개
   1. 장르 개수가 3개인지 검증
@@ -70,14 +82,17 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
       /* 선택한 장르가 현재 배열에 있는지 확인하고,
        배열에 있는 장르와 다르면(true) arrayGenres에 추가(filter로 반환됨), 
        같다면(false) 삭제 */
-      if (arrayGenres.includes(genre)) {
+      const translatedGenre = genreMap[genre];
+      if (!translatedGenre) return arrayGenres;
+
+      if (arrayGenres.includes(translatedGenre)) {
         console.log("장르 다시 추가할거임.");
-        return arrayGenres.filter((g) => g !== genre);
+        return arrayGenres.filter((g) => g !== translatedGenre);
       }
       // 선택된 장르가 3개 미만이면 새로운 장르 추가
       if (arrayGenres.length < 3) {
         console.log("첫 번째 if 문 통과! 두 번 째 if문 시작");
-        return [...arrayGenres, genre];
+        return [...arrayGenres, translatedGenre];
       } else {
         console.log(arrayGenres);
         return arrayGenres;
@@ -85,19 +100,62 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
     });
   };
 
-  const onChangeFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const file = event.target.files?.[0];
-    if (file) {
-      console.log("선택된 파일:", file);
+  const onChangeMP3 = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const selectedFile = event.target.files?.[0];
+
+    if (selectedFile) {
+      console.log("선택된 파일:", selectedFile);
+
+      // 파일 이름 중복 검사
+      const isDuplicate = songs.some(
+        (song) => song.originFilename === selectedFile.name
+      );
+      if (isDuplicate) {
+        alert("이미 같은 파일 이름을 가진 곡이 존재합니다.");
+
+        //파일 입력 상태 초기화
+        setFile(null);
+        setOriginFilename("");
+        event.target.value = ""; // 파일 선택 input 초기화
+        return;
+      }
+
+      // 파일 유효성 검사 -> isValid = false>true
+      const isValidFile = checkValidationFile(selectedFile);
+
+      if (isValidFile) {
+        setFile(selectedFile);
+        setOriginFilename(selectedFile.name); // 파일 이름 추가
+        console.log("*********************파일 이름", selectedFile.name); // 파일 이름 추가
+        return;
+      }
     }
-    /*검증 로직
-        !isValid => false로 오면 여기서 끝냄.
-        !isValid => true로 오면 다음 로직 실행
-    */
-    const isValid = checkValidationFile(file);
-    if (!isValid) return;
     // !isValid => true 인 경우
     // 제출 버튼 활성화 시키기!!
+  };
+
+  //업로드 할 음원 파일 검증 로직
+  const checkValidationFile = (file?: File): boolean => {
+    // 선택된 파일이 없을 때
+    if (typeof file === "undefined") {
+      alert("선택된 음원 파일이 없습니다!!");
+      setFile(null);
+      setOriginFilename("");
+      return false;
+    }
+    // 음원 파일 용량 검증 (MB = 1024*1024)
+    if (file.size > 1 * 1024 * 1024) {
+      alert("파일 용량이 너무 큽니다. (제한: 1MB)");
+      setFile(null);
+      setOriginFilename("");
+      return false;
+    }
+    // 음원 파일 형식 검증 -  MP3 파일만 허용
+    //  if (!file.type.includes("audio/mp3")) {
+    //   alert("MP3 파일만 업로드 가능합니다.");
+    //   return false;
+    // }
+    return true;
   };
 
   /* 제출 버튼 누르면
@@ -109,27 +167,36 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
       alert("최대 10개의 곡만 추가할 수 있습니다.");
       return;
     }
+
     const sendSong: Song = {
-      title,
+      songName,
       lyricist,
-      songwriter,
+      composer,
       lyrics,
       genres,
-      mp3File,
+      originFilename,
+      file,
     };
     const updatedSongs = [...songs, sendSong];
     // setSongs([...songs, sendSong]);
     setSongs(updatedSongs);
-    onSongsChange(updatedSongs); // 부모 컴포넌트로 데이터 전달
 
-    setTitle("");
+    console.log("곡 업데이트 됨");
+    onSongsChange(updatedSongs); // 부모 컴포넌트로 데이터 전달
+    console.log("부모(page.tsx)로 전달됨");
+    console.log("요소 초기화 전");
+
+    //전달 후 값들 초기화
+    setSongName("");
     setLyricist("");
-    setSongwriter("");
+    setComposer("");
     setLyrics("");
     setGenres([]);
-    setMp3File(null);
-
+    setOriginFilename("");
+    setFile(null);
     closeModal();
+
+    console.log("요소 초기화 후");
   };
 
   // 삭제 버튼 클릭 시 songs 배열에서 삭제
@@ -189,8 +256,8 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
                         이름
                         <input
                           type="text"
-                          value={title}
-                          onChange={(e) => setTitle(e.target.value)}
+                          value={songName}
+                          onChange={(e) => setSongName(e.target.value)}
                           className={styles.text__input}
                           required
                         />
@@ -218,8 +285,8 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
                           작곡가
                           <input
                             type="text"
-                            value={songwriter}
-                            onChange={(e) => setSongwriter(e.target.value)}
+                            value={composer}
+                            onChange={(e) => setComposer(e.target.value)}
                             className={styles.text__input}
                             required
                           />
@@ -278,7 +345,7 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
                         <input
                           type="file"
                           accept="audio/*"
-                          onChange={onChangeFile}
+                          onChange={onChangeMP3}
                           className={styles.file__input}
                           required
                         />
@@ -334,7 +401,7 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
               <Image src={musicIcon} alt="delete" width={30} height={30} />
             </div>
             <div className={styles.songInfo}>
-              <strong>곡 이름:</strong> {song.title}, <strong>장르:</strong>{" "}
+              <strong>곡 이름:</strong> {song.songName}, <strong>장르:</strong>{" "}
               {song.genres.join(", ")}
             </div>
             <button
@@ -349,19 +416,3 @@ export default function SongForm({ onSongsChange }: SongFormProps) {
     </div>
   );
 }
-
-//업로드 할 음원 파일 검증 로직
-const checkValidationFile = (file?: File): boolean => {
-  // 선택된 파일이 없을 때
-  if (typeof file === "undefined") {
-    alert("선택된 음원 파일이 없습니다!!");
-    return false;
-  }
-  // 음원 파일 용량 검증 (MB = 1024*1024)
-  if (file.size > 1 * 1024 * 1024) {
-    alert("파일 용량이 너무 큽니다. (제한: 1MB)");
-    return false;
-  }
-  // 음원 파일 형식 검증 ------뭘 제한할 지 모르겠음!!!!!!!!!!
-  return true;
-};
