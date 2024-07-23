8757cc87a57143c7cd7370639a4f943cd68f40e9
G1Coding <32190250@dankook.ac.kr>
2024-07-23 20:43:06 +0900

    [서버 연동 완료] 앨범 상세 페이지 완성(UI 수정해야함)

--- front/src/app/(_Front)/details/album/[id]/page.tsx
+++ front/src/app/(_Front)/details/album/[id]/page.tsx
@@ -1,63 +1,65 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import Image from "next/image";
-import MusicContentInfo from "@/app/components/music-content/MusicContentInfo";
 import Link from "next/link";
-import { HeartButton } from "@/app/components/button/commonButton";
+import MusicList from "@/app/components/main/MusicList";
+import { useParams } from "next/navigation";
 
-const album = () => {
-  const introduction = {
-    title: "소개글",
-    text: `aespa, 첫 정규 앨범 ‘Armageddon’ 발매! 힙한 무드 ‘Supernova’→힙합
-    댄스곡 ‘Armageddon’으로 강렬한 질주! 세계관 시즌 2 서사 담은 역대급
-    스케일 음악+비주얼! ‘글로벌 히트메이커’ aespa가 첫 정규 앨범
-    ‘Armageddon’을 발매했다.`,
-  };
+type SongData = {
+  nickname: string;
+  songId: number;
+  title: string;
+}
+type AlbumData = {
+  name: string;
+  albumImg: string;
+  nickname: string;
+  intro: string;
+  songs: Array<SongData>;
+};
+type Params = {
+  id: string;
+};
 
-  const lyrics = {
-    title: "가사",
-    text: `  like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야 like some kind of Supernova Watch out Look at me go 재미 좀 볼 빛의
-          Core So hot hot 문이 열려 서로의 존재를 느껴 마치 Discord 날 닮은 너
-          너 누구야`,
-  };
+export default function Album() {
+  const params = useParams() as Params;
+  const { id } = params;
+  console.log("params는", params);
 
-  const albumInfo = {
-    title: "앨범 제목",
-    artist: "아이유",
-    genre: "발라드, 케이팝",
-  };
+  const [isModalOpen, setModalOpen] = useState(false);
 
-  const likeNum = 30;
+  const handleOpenModal = () => setModalOpen(true);
+  const handleCloseModal = () => setModalOpen(false);
+
+  const [albumData, setAlbumData] = useState<AlbumData | null>(null);
+  console.log(albumData?.songs);
+
+
+
+  useEffect(() => {
+    if (id) {
+      const fetchSongData = async () => {
+        try {
+          const response = await fetch(`http://localhost:8080/album/${id}`);
+          const data: AlbumData = await response.json();
+          setAlbumData(data);
+        } catch (error) {
+          console.error("앨범데이터 정보를 가져오는 데 실패했습니다.", error);
+        }
+      };
+
+      fetchSongData();
+    }
+  }, [id]);
+
+  if (!albumData) return <div>Loading...</div>;
+
+  console.log("음악 정보들",albumData.songs);
+
 
   return (
     <div className={styles.container}>
-      음악 사진 눌렀을 때 나오는 상세 페이지 입니다.
       <section className={styles.music__info__section}>
         <h2>앨범 정보</h2>
 
@@ -66,45 +68,34 @@ const album = () => {
             <div className={styles.album__img}>
               <Image
                 src="/svgs/album_thumb.png"
-                alt="Services"
+                alt="Album Thumbnail"
                 width={130}
                 height={130}
               />
             </div>
             <div className={styles.song__info}>
               <div className={styles.title__artist}>
-                <h1>{albumInfo.title}</h1>
+                <h1 onClick={handleOpenModal} style={{ cursor: "pointer" }}>
+                  {albumData.name}
+                </h1>
                 <h2>
-                  <Link href="./">{albumInfo.artist}</Link>
+                  <Link href="./">{albumData.nickname}</Link>
                 </h2>
-                <div>11곡</div>
+                <div>{albumData.songs.length} 곡</div>
               </div>
-
-    
             </div>
-
-            <button className={styles.play__btn}>
-              <Image
-                src="/svgs/play_btn.svg"
-                alt="Services"
-                width={60}
-                height={60}
-              />
-            </button>
           </div>
 
-          <div className={styles.divider}> </div>
+          <div className={styles.divider}></div>
 
           <div className={styles.btn__container}>
-        
-
             <div className={styles.btn__share}>
               <span>공유</span>
               <span>
                 <a href="#">
                   <Image
                     src="/social/kakao.png"
-                    alt="Services"
+                    alt="Kakao"
                     width={40}
                     height={40}
                   />
@@ -112,7 +103,7 @@ const album = () => {
                 <a href="#">
                   <Image
                     src="/social/instagram.png"
-                    alt="Services"
+                    alt="Instagram"
                     width={40}
                     height={40}
                   />
@@ -122,12 +113,70 @@ const album = () => {
           </div>
         </div>
       </section>
-      <section className={styles.lyrics__section}>
-        {/* <MusicContentInfo title={introduction.title} text={introduction.text} /> */}
-        <MusicContentInfo title={lyrics.title} text={lyrics.text} />
+
+
+      <section>
+        <h2>수록곡</h2>
+
+        <div className={styles.container}>
+      <div className={styles.play__option__container}>
+        <a href="#">
+          <button>전체 재생</button>
+        </a>
+        <a href="#">
+          <button>선택 재생</button>
+        </a>
+      </div>
+
+      <div className={styles.music__chart__container}>
+        <table className={styles.table__container}>
+          <thead>
+            <tr className={styles.blind}>
+              <th>체크박스</th>
+              <th>썸네일</th>
+              <th>랭킹</th>
+              <th>음악이름</th>
+              <th>가수 이름</th>
+              <th>재생</th>
+              <th>옵션</th>
+            </tr>
+          </thead>
+          <tbody className={styles.table__row}>
+            {albumData.songs ? (
+              albumData.songs.map((songs, index) => (
+                <MusicList
+                  key={songs.songId}
+                  musicChartData={{
+                    songId: songs.songId,
+                    albumImg: albumData.albumImg,
+                    title: songs.title,
+                    nickname: songs.nickname  
+                  }}
+                  index={index}
+                  showCheckbox={true}
+                />
+              ))
+            ) : (
+              <tr>
+                <td colSpan={7}>데이터를 불러올 수 없습니다.</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
+    </div>
+    
       </section>
+    
+
+      <div className={`${styles.modal} ${isModalOpen ? styles.open : ""}`}>
+        <div className={styles.modal__content}>
+          <button className={styles.close__btn} onClick={handleCloseModal}>
+            X
+          </button>
+          {/* <MusicContentInfo title={albumData.name} text={albumData.intro} /> */}
+        </div>
+      </div>
     </div>
   );
-};
-
-export default album;
+}
